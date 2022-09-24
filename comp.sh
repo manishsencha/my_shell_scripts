1bf511a5e0ec739af1500bfac14240aa0c1d87a5
Manish Sencha <manishsencha2001@gmail.com>
2022-09-24 21:53:43 +0530

    improved file structure for easy use

--- comp.sh
+++ comp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script is competitive helper to easily initiate a new filei
+# This script is competitive helper to easily initiate a new file
 
 template_dir=$HOME/templates
 
@@ -9,11 +9,12 @@ current_dir=$PWD
 filename=$1
 
 create() {
-    if[ -z "$filename" ]
+
+    if [ -z "$filename" ]
     then
         echo "Please provide filename as input..."
         return
     fi
-    cp $template_dir/template.cpp $current_dir/$1
+    cp $template_dir/template.cpp $current_dir/$filename
 }
 create
